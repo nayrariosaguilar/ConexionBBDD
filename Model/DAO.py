4f88b2bc4e4e2c376a107911820d09097e4b888d
nayrariosaguilar <nayrios2004@gmail.com>
2025-02-09 13:28:45 +0100

    dos formas de conectar con la bbdd

--- Model/DAO.py
+++ Model/DAO.py
@@ -1,24 +1,21 @@
 import psycopg2
 from psycopg2 import Error
 
-
-#Obrim una excepció per un intent de connexió a la base de dades
 try:
-    # Passem el servidor, el nom de la base de dades, l'usuari i el seu password
     connection = psycopg2.connect(user="postgres",
-                                  password="1234",
+                                  password="postgres",
                                   host="127.0.0.1",
                                   port="5432",
                                   dbname="training")
 
-    # Create a cursor to perform database operations
     cursor = connection.cursor()
-    # Print PostgreSQL details
     print("PostgreSQL server information")
     print(connection.get_dsn_parameters(), "\n")
-    # Executing a SQL query
 
-    # Fetch result
+    # Ejecutar una consulta SQL
+    cursor.execute("SELECT version();")
+
+    # Obtener el resultado
     record = cursor.fetchone()
     print("You are connected to - ", record, "\n")
 
